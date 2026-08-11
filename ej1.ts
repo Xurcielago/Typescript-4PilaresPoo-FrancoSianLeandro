89bcc8d408b9efe2e7979d40eacfb0d952110804
Xurcielago <therouses00@gmail.com>
2026-08-11 16:46:28 -0300

    feat (ej1 abstracción): Modelada clase Producto para el sistema de ventas con sus respectivos atributos y métodos

--- ej1.ts
+++ ej1.ts
@@ -1,1 +1,33 @@
 //Ejercicio 1 — Abstracción
+class Producto {
+    nombre: string
+    precio: number
+    categoria: string
+    stock: number
+    constructor (nombre: string, precio: number, categoria: string, stock: number) {
+        this.nombre = nombre
+        this.precio = precio
+        this.categoria = categoria
+        this.stock = stock
+    }
+    describir(): string {
+        return `${this.nombre} (${this.categoria}): $${this.precio} — ${this.stock} unidades`
+    }
+    hayStock(cantidad: number): boolean {
+        if ((this.stock-cantidad) >= 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+    venderUnidades(cantidad: number): void {
+        if (!this.hayStock(cantidad)) {
+            throw new Error(`Stock insuficiente para vender ${cantidad} unidades de "${this.nombre}"`);
+        }
+        this.stock -= cantidad;
+        //Se lanza un error y se detiene el flujo porque de otra forma podría afectar al funcionamiento del negocio.
+    }
+    aplicarDescuento(porcentaje: number): number {
+        return this.precio * (1 - porcentaje / 100);
+    }
+}
